ba297809e7163d9a061137e345ec1b9c89ac7443
fatihkayas <seavsdeep@gmail.com>
2022-12-29 23:05:57 +0000

    added scripts for running dummy selenium job

--- ansible/scripts/run_dummy_selenium_job.sh
+++ ansible/scripts/run_dummy_selenium_job.sh
@@ -1,2 +1,2 @@
-PATH="$PATH:/usr/local/bin"
+PATH="$PATH:/usr/local/bin" # important
 ansible-playbook --connection=local --inventory 127.0.0.1, --extra-vars "workspace=${WORKSPACE}" ./ansible/playbooks/pb_run_dummy_selenium_job.yaml
